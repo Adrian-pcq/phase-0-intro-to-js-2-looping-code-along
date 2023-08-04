60b7a01dbac32d8aefae7359934f5ac01a746c88
Adrian <adrianqueizan@gmail.com>
2023-08-04 17:23:28 -0400

    creats loops

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const card = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(array,event){
+    let newarray = []
+for (let i = 0; i< array.length; i++){
+   newarray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+
+    }return newarray
+}
+writeCards(["Guadalupe", "Ollie", "Aki"],surprise);
+
+function countDown(){
+    let i = 10
+    while (i >= 0){
+        console.log(i--)
+    }
+}
